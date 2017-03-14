6b78fc89ee83baa2d804582fb0e61b2e3e6e7ab1
Huy Le <huylx86@gmail.com>
2017-03-14 15:12:38 +0700

    - fix splash creen difference screen size

--- LF_WMI/app/src/main/java/com/wmi/finedu/utils/Constants.java
+++ LF_WMI/app/src/main/java/com/wmi/finedu/utils/Constants.java
@@ -9,9 +9,9 @@ public class Constants {
     public static String VIDEOS_TITLE = "Videos";
     public static String ABOUT_US_TITLE = "About Us";
 
-    public static String HOME_URL = "http://inkstudiowp.16mb.com/wmi/mobile/";
-    public static String VIDEO_URL = "http://inkstudiowp.16mb.com/wmi/video/";
-    public static String ABOUT_US_URL = "http://inkstudiowp.16mb.com/wmi/credits/";
+    public static String HOME_URL = "http://wmi.learnflux.net/mobile/";//"http://inkstudiowp.16mb.com/wmi/mobile/";
+    public static String VIDEO_URL = "http://wmi.learnflux.net/video/";//http://inkstudiowp.16mb.com/wmi/video/";
+    public static String ABOUT_US_URL = "http://wmi.learnflux.net/credits/";//"http://inkstudiowp.16mb.com/wmi/credits/";
 
     public static String CONTACT_US_URL = "http://www.wmi.com.sg/contact-us.html";
     public static String FACEBOOK_URL = "https://www.facebook.com/WMIsg/photos/a.567627296644102.1073741825.142122185861284/1406339156106241/?type=3&theater";
