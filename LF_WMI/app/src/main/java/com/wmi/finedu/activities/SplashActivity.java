f7334e6b9bccd8c10885a2753c362feb532026cc
Huy Le <huylx86@gmail.com>
2017-03-18 12:21:15 +0700

    - fix font size and tap to go in Home screen from splash screen

--- LF_WMI/app/src/main/java/com/wmi/finedu/activities/SplashActivity.java
+++ LF_WMI/app/src/main/java/com/wmi/finedu/activities/SplashActivity.java
@@ -5,23 +5,35 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.wmi.finedu.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private boolean mIsFinish = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        View main = findViewById(R.id.ln_main);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mHandler.sendEmptyMessage(0);
+            }
+        });
         mHandler.sendEmptyMessageDelayed(0, 3000);
     }
 
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            finish();
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            if(!mIsFinish) {
+                mIsFinish = true;
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
             super.handleMessage(msg);
         }
     };
